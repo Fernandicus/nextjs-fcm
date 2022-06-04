32fbb32eb9141abc43c0845824fff0b1d4e70939
Fernandicus <80597450+Fernandicus@users.noreply.github.com>
2022-06-04 15:55:35 +0200

    refact

--- services/firebase-permits.js
+++ services/firebase-permits.js
@@ -1,16 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getToken, getMessaging, onMessage } from "firebase/messaging";
-//import firebaseConfig from "./firebase-config";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyB-DWVmONlD4kTc92lenN7XeoTTsuv7IKg",
-  authDomain: "fir-cloud-messaging-3fd3a.firebaseapp.com",
-  projectId: "fir-cloud-messaging-3fd3a",
-  storageBucket: "fir-cloud-messaging-3fd3a.appspot.com",
-  messagingSenderId: "948741768361",
-  appId: "1:948741768361:web:37a4cff14da9592e2595b8",
-  measurementId: "G-HCYNE9SW5R",
-}
+import firebaseConfig from "./firebase-config";
 
 const app = initializeApp(firebaseConfig);
 
